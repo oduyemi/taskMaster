b8a8d589b0ebc08384af4366dbee4ab8c556c168
tosmel2 <adewaletosin0808@gmail.com>
2024-04-24 17:40:17 +0100

    feat: frontend setup

---
+++ client/src/App.jsx
@@ -0,0 +1,27 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+// import TaskOverview from './pages/TaskOverview';
+// import PrivateRoute from './components/PrivateRoute';
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        {/* <Route element={<PrivateRoute />}>
+          <Route path='/task-overview' element={<TaskOverview />} />
+        </Route> */}
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+
+const Home = () => {
+  return (
+    <div>
+        <h1 className="p-5 text-3xl font-bold text-white underline bg-orange-700">
+        Hello TaskMaster!
+      </h1>
+    </div>
+  )
+}
+
+export default Home
